d1121c297be0244fedf999f58c21adfdee52645c
Manianbu5700 <manianbu5700@gmail.com>
2026-03-24 12:14:09 +0530

    modified

--- tests/Alert.spec.js
+++ tests/Alert.spec.js
@@ -20,7 +20,7 @@ test('Alert Handling', async ({ page }) => {
 
     await page.once('dialog', async (dialog) => {
         await console.log("Prompt Alert :", dialog.message())
-        await dialog.accept('hii')
+        await dialog.accept('manianbu')
     })
 
     await page.locator('//button[@class="btn btn-info"]').click()
